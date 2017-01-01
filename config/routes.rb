796b8d888a24dab4122f44f1df71cb11958f7980
hukui <tckuikui1234>
2017-01-02 06:22:25 +0800

    jion & quit group action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "groups#index"
   resources :groups do
+    member do
+      post :join
+      post :quit
+    end
     resources :posts
   end
+    root "groups#index"
 end
